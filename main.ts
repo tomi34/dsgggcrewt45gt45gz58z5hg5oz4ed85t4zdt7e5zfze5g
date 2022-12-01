1c357f773fdc706a60e615c0873d59f3de489642
tomi34 <66636695+tomi34@users.noreply.github.com>
2022-12-01 12:30:21 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 4; index++) {
+        led.plot(index - 1, 0)
+        led.plot(3, index)
+        led.plot(index - 1, 4)
+    }
 })
